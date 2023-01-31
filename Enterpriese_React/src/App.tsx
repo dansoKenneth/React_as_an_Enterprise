590bdbcfed522dce20666f2f1dff2aff6f59d6c9
HighestDev <kenneth.danso@amalitech.org>
2023-01-31 17:10:18 +0000

    Structure of project

---
+++ Enterpriese_React/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-white flex items-center justify-center max-w-[1240px] mx-auto h-24'>
+       <h1 className='text-[#03fa] uppercase font-bold text-3xl w-full'>react.</h1>
+       <ul className='flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Portfolio</li>
+        <li className='p-4'>Research</li>
+
+       </ul>
+      </div>
+  )
+}
+
+export default App
---
+++ Enterpriese_React/src/model.ts
@@ -0,0 +1,31 @@
+export interface CardTypes{
+  
+//   Titles
+  title?:string,
+  title1?:string,
+  title2?:string,
+
+// Base
+   base?:string,
+   base1?:string,
+   base2?:string,
+
+   Image?:any,
+}
+
+
+
+export interface GetShortenLinkTypes{
+  result:{
+    code:string,
+    original_link:string,
+    full_short_link:string,
+    short_link:string
+  }
+}
+
+
+
+
+
+
